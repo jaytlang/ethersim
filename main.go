jaytlang/ethersim

commit 851278a44ecabae1b4747f1af9b69708424667e1

Prepare for CRS by passing *net.Conn to broadcast
The broadcast method now doesn't send messages back to a server
which originally sent them. This is far superior to the original
model in terms of ease of use while still staying relatively true
to the ethernet model.

The rest is pretty straight forward. We should be able to just hook
up a channel between the tap's send/recv and issue a kill signal if
we see problems.

Signed-off-by: Jay Lang <jaytlang@mit.edu>

main.go
@@ -21,7 +21,7 @@ func main() {
 	if conf.Serving {
 		sessionStart := strings.LastIndex(conf.Name, "/") + 1
 		sessionID := conf.Name[sessionStart:]
-		fmt.Printf("Good to go! Now run `ethersim %s`\n", sessionID)
+		fmt.Printf("Good to go! Now run `ethersim -c %s`\n", sessionID)
 		media.ServeCon(&conf)
 
 	} else {

media/srv.go
@@ -7,6 +7,11 @@ import (
 	"unicode/utf8"
 )
 
+type traceData struct {
+	r   rune
+	con *net.Conn
+}
+
 // The following are input and broadcast
 // operations for our one-to-many setup here.
 // Basically, if one client we're connected to
@@ -17,7 +22,7 @@ import (
 // the l.l. locking things we teach...maybe looking
 // at this code isn't too instructive. However, for this
 // use case, CSP is REALLY REALLY nice.
-func input(con *net.Conn, c chan<- rune) {
+func input(con *net.Conn, c chan<- traceData) {
 	for {
 		// Receive a new message, probably
 		// in the form of a 4-byte rune
@@ -36,11 +41,11 @@ func input(con *net.Conn, c chan<- rune) {
 		}
 
 		// Send it along!
-		c <- r
+		c <- traceData{r: r, con: con}
 	}
 }
 
-func broadcast(dc <-chan rune, cc <-chan *net.Conn) {
+func broadcast(dc <-chan traceData, cc <-chan *net.Conn) {
 	cons := []*net.Conn{}
 
 	for {
@@ -52,11 +57,14 @@ func broadcast(dc <-chan rune, cc <-chan *net.Conn) {
 			cons = append(cons, newcon)
 
 		// If there's new data, send it along
-		case r := <-dc:
+		case td := <-dc:
 			bytes := make([]byte, 4)
-			utf8.EncodeRune(bytes, r)
+			utf8.EncodeRune(bytes, td.r)
 
 			for i, c := range cons {
+				if c == td.con {
+					continue
+				}
 				_, err := (*c).Write(bytes)
 				if err != nil {
 					(*c).Close()
@@ -76,7 +84,7 @@ func ServeCon(c *common.Conf) {
 		log.Fatalf("Fatal error starting ethersim server: %s", err.Error())
 	}
 
-	dc := make(chan rune)
+	dc := make(chan traceData)
 	cc := make(chan *net.Conn)
 
 	// Start the broadcaster

tap/tap.go
@@ -39,13 +39,12 @@ func doOutput(c *net.Conn) {
 		}
 
 		if !isReceiving {
-			isReceiving = true
 			fmt.Print("Receiving: ")
+			isReceiving = true
 		}
 
 		if r == '✓' {
 			isReceiving = false
-			fmt.Println()
 		} else {
 			fmt.Printf("%c", r)
 		}
@@ -56,8 +55,13 @@ func doOutput(c *net.Conn) {
 func doInput(c *net.Conn) {
 	for {
 		str := readMsgChars()
+		if str == "\n" {
+			continue
+		}
+
 		fmt.Print("Sending... ")
 		b := make([]byte, 4)
+
 		for _, r := range str {
 			fmt.Printf("%c", r)
 			utf8.EncodeRune(b, r)

commit f0b0c80a5e45dc5f4f87e7cce2c1c9c6c5ed1b93

Output ether ID instead of command
Make a minor wording change to the message passed by the server side
of Ethersim, to line up closer to the lab manual and ease understanding.

Reviewed-by: Jay Lang <jaytlang@mit.edu>
Fixes: 851278a44eca ("Prepare for CRS by passing *net.Conn to broadcast")

main.go
@@ -21,7 +21,7 @@ func main() {
 	if conf.Serving {
 		sessionStart := strings.LastIndex(conf.Name, "/") + 1
 		sessionID := conf.Name[sessionStart:]
-		fmt.Printf("Good to go! Now run `ethersim -c %s`\n", sessionID)
+		fmt.Printf("Good to go! Your ether ID is %s.\n", sessionID)
 		media.ServeCon(&conf)
 
 	} else {